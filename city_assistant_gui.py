87413935bfffa434623bf6f42c8de09fbb7106a7
Yigit Atay <yigitatay@Yigits-MacBook-Pro.local>
2025-02-12 22:50:24 +0100

    More refactoring and comments

--- city_assistant_gui.py
+++ city_assistant_gui.py
@@ -11,20 +11,19 @@ class ChatApp:
     def __init__(self, root):
         self.root = root
         self.root.title("City Assistant")
-        self.root.configure(bg="#222")  # Set background color to dark gray
+        self.root.configure(bg="#222")  # dark gray
 
-        # Initialize a list to store chat history
         self.chat_history = []
 
-        # Create a text widget to display chat messages
+        # Text widget to display chat messages
         self.chat_display = tk.Text(root, width=70, height=50, bg='white')
         self.chat_display.pack()
 
-        # Create an entry widget for user input
+        # Entry widget for user input
         self.user_input = tk.Entry(root, width=50)
         self.user_input.pack()
 
-        # Create a send button to send user messages
+        # Send button to send user messages
         self.send_button = tk.Button(root, text="Send", command=self.send_message, bg="#444", fg="black")
         self.send_button.pack()
 
@@ -33,25 +32,20 @@ class ChatApp:
         self.chat_history.append(initial_message)
         self.display_message(initial_message, color="blue")
 
-        # Set a flag to track whether the model has been selected
-        self.model_selected = False
-        self.model_selection_attempts = 0  # Count user's attempts to enter the model name
-
         # Track whether the user input is enabled
         self.user_input_enabled = True
-
-        self.reported = False
-
         # Track whether the user should be able to send a message 
         self.send_enabled = True
 
-        # Bind the close event to the save_and_exit function
+        # Allow reporting of responses
+        self.reported = False
+
         self.root.protocol("WM_DELETE_WINDOW", self.save_and_exit)
 
-        # Bind the Enter key to call the send_message function
+        # Bind the Enter key to send_message
         self.user_input.bind("<Return>", lambda event: self.send_message())
     
-         # Initialize the City Assistant chatbot
+         # Initialize
         self.city_assistant = CityAssistant(self.chat_display)
 
     def save_and_exit(self):
@@ -59,16 +53,15 @@ class ChatApp:
         if not self.reported:
             self.save_chat_history(report=False)  # Save chat history before exiting
 
-        self.root.destroy()  # Close the application
+        self.root.destroy()
 
     def send_message(self):
         if self.send_enabled:
             user_message = self.user_input.get()
             if user_message and len(user_message) > 0:
                 self.chat_history.append(f"User: {user_message}")
-                self.display_message(f"User: {user_message}", color="black")  # Set User message to black
+                self.display_message(f"User: {user_message}", color="black")
 
-                # Clear the user input field
                 self.user_input.delete(0, tk.END)
 
                 processing_message = "City Assistant: ..."
@@ -94,29 +87,27 @@ class ChatApp:
         # Get the bot's response based on user input
         bot_message = self.bot_answer(user_message)
 
+        # City Assistant is prompted to add something like "not sure" to its response if not sure
+        # TODO: This could be improved by adding a flag to the bot answer
         if "not sure" in bot_message.lower() or "not applicable" in bot_message.lower():
             customer_agent = True
             
-
         self.send_button['state'] = 'active'
         self.send_enabled = True
         ## We don't need to show the user the request type
         bot_message = bot_message.split('Used request type')[0]
         self.display_message(bot_message, color='blue')
         self.chat_history.append(bot_message)
-
-        
+ 
         if customer_agent:
             self.reported = True
             self.save_chat_history(report=True)
 
 
     def initialize_chat(self):
-        # Initialize the City Assistant chatbot
         self.city_assistant = CityAssistant(self.chat_display)
 
     def bot_answer(self, user_input):
-        # Use the City Assistant instance to get a response based on user input
         bot_response = self.city_assistant.run(user_input)
         return f"City Assistant: {bot_response}"
 
@@ -128,13 +119,12 @@ class ChatApp:
             report_button = tk.Button(self.root, text="Report", bg="#FF3131", fg="#000000", relief=tk.FLAT, font=("Arial", 12), command=lambda message=message: self.report_message())
             report_button.pack(pady=1)
 
-            report_button.config(borderwidth=1, relief="solid")  # Adjust border width and relief
+            report_button.config(borderwidth=1, relief="solid")
             report_button.config(highlightbackground="#F1F1F1", highlightcolor="#F1F1F1", highlightthickness=1, activebackground="#C0C0C0", activeforeground="#000000")            
             self.chat_display.window_create(tk.END, window=report_button)
 
-
         self.chat_display.insert(tk.END, message + "\n", color)
-        self.chat_display.insert(tk.END, "\n\n")  # Add some spacing
+        self.chat_display.insert(tk.END, "\n\n")
 
         last_index = self.chat_display.index(tk.END)
         self.chat_display.see(tk.END)
